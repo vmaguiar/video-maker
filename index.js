125e7e25d9b622aadefa77960e9e61455c2f1bfa
vmaguiar <vmaguiar@hotmail.com>
2020-05-30 09:32:38 -0300

    feat: add search term and prefix

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 const readline = require('readline-sync')
 
 function start(){
-    content = {}
+    const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm(){
         return readline.question('Type a Wikipidia search term: ')
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is', 'What is', 'The history of']
+        // Readline organiza os prefixos, porem so seleciona a posição no array //
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
     }
 
     console.log(content)
